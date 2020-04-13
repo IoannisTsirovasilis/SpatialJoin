467cbf23b42d38ae477531163abc5c2c6692d12f
Ioannis Tsirovasilis <ioannistsirovasilis@gmail.com>
2020-04-13 14:29:47 +0300

    Added min/max coords parser tests for special cases

--- src/test/java/gr/unipi/spatialJoin/MinMaxCoordsParserTest.java
+++ src/test/java/gr/unipi/spatialJoin/MinMaxCoordsParserTest.java
@@ -20,7 +20,7 @@ public class MinMaxCoordsParserTest
     }
 
     
-    public void testParserHotelRecordReturnsCorrectCoords() {
+    public void testParserCoordsInBetweenRecordReturnsCorrectCoords() {
 		Text record = new Text("3|Alameda County Fairgrounds|0|28|37.66169|-121.887367|laundry_service,meeting_rooms,event_catering,complimentary_breakfast,internet,internet_wireless,pets,fitness_facilities");
 		int latitudeIndice = 4;
 		int longitudeIndice = 5;
@@ -29,5 +29,27 @@ public class MinMaxCoordsParserTest
 		parser.parse(record, latitudeIndice, longitudeIndice, separator);
 		assertEquals(37.66169, parser.getLatitude());
 		assertEquals(-121.887367, parser.getLongitude());
+	}
+    
+    public void testParserCoordsAtBeginningOfRecordReturnsCorrectCoords() {
+		Text record = new Text("34.016384|12|Starbucks Coffee|4|-118.49698|American, Coffee");
+		int latitudeIndice = 0;
+		int longitudeIndice = 4;
+		String separator = "|";
+		MinMaxCoordsParser parser = new MinMaxCoordsParser();
+		parser.parse(record, latitudeIndice, longitudeIndice, separator);
+		assertEquals(34.016384, parser.getLatitude());
+		assertEquals(-118.49698, parser.getLongitude());
+	}
+    
+    public void testParserCoordsAtEndOfRecordReturnsCorrectCoords() {
+		Text record = new Text("12|Starbucks Coffee|4|-118.49698|American, Coffee|34.016384");
+		int latitudeIndice = 5;
+		int longitudeIndice = 3;
+		String separator = "|";
+		MinMaxCoordsParser parser = new MinMaxCoordsParser();
+		parser.parse(record, latitudeIndice, longitudeIndice, separator);
+		assertEquals(34.016384, parser.getLatitude());
+		assertEquals(-118.49698, parser.getLongitude());
 	}
 }
